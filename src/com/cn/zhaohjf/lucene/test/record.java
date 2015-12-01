65389f91617876dd2862f097161357793ba0cc38
zhaohjf <zhaohj0315@163.com>
2015-12-01 20:33:32 +0800

    Git客户端
    Change-Id: I4bc3d506f6aef4b877b5e5abdc7f033660eda136
    Signed-off-by: zhaohjf <zhaohj0315@163.com>

--- src/com/cn/zhaohjf/lucene/test/record.java
+++ src/com/cn/zhaohjf/lucene/test/record.java
@@ -4,6 +4,8 @@ package com.cn.zhaohjf.lucene.test;
  * http://blog.csdn.net/isea533/article/details/48791309
  * http://www.ibm.com/developerworks/cn/java/j-lo-lucene1/
  *
+ * https://code.google.com/p/msysgit/downloads/list
+ *
  */
 public class record {
 
